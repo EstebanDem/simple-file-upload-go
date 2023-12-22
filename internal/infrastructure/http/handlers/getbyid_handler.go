7dbdf72576cbf7a31f0df218454feb598dabee66
Esteban Demarchi <estebanjosedemarchi@gmail.com>
2023-12-22 20:05:39 -0300

    Improve response when getting by id

--- internal/infrastructure/http/handlers/getbyid_handler.go
+++ internal/infrastructure/http/handlers/getbyid_handler.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"github.com/estebandem/simple-file-upload-go/internal/application/dtos/responses"
 	"github.com/estebandem/simple-file-upload-go/internal/application/usecases"
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
@@ -10,20 +11,24 @@ import (
 
 func NewGetByIdHandler(guc usecases.GetByIdUseCase) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
 		idStr := mux.Vars(r)["id"]
 
 		id, err := uuid.Parse(idStr)
 
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(err)
+			json.NewEncoder(w).Encode(responses.JsonResponse{Message: "invalid uuid", Status: http.StatusBadRequest})
+			return
 		}
 
 		file, err := guc.GetById(id)
 
 		if err != nil {
 			w.WriteHeader(http.StatusNotFound)
-			json.NewEncoder(w).Encode(err)
+			json.NewEncoder(w).Encode(responses.JsonResponse{Message: err.Error(), Status: http.StatusBadRequest})
+			return
 		}
 
 		contentType := http.DetectContentType(file)
